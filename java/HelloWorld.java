2085e34225e3341c01ab168f1d86a4127bc02c33
Nickolai <nick.rebez@gmail.com>
2019-11-01 23:01:35 -0400

    Start of section 3

--- java/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("New File");
-        Test();
-    }
-    public static void Test(){
-        System.out.println("Viola");
-    }
-   }
---
+++ java/sec3/Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+   public static void main(String[] args){
+       System.out.println("Hello, Nick");
+       int myFirstNum = 5;
+   } 
+}
