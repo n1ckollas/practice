d6fbf810a22b59c63d3f169f67bafbd9f07fd2b2
Nickolai <nick.rebez@gmail.com>
2019-11-04 17:43:35 -0500

    UP until floating numbers

---
+++ java/src/sec3/Hello.java
@@ -0,0 +1,61 @@
+
+public class Hello{
+   public static void main(String[] args){
+        System.out.println("Hello, Nick");
+        int myFirstNum = 5; // 32 bits hense the width of 32
+
+        System.out.println(myFirstNum);
+        System.out.println("whatever");
+        int mySecondNum = (10 + 5) + (2 * 10);
+        System.out.println(mySecondNum);
+        int myThirdNum = 6;
+        int myTotal = myFirstNum + mySecondNum + myThirdNum;
+        System.out.println(myTotal);
+        int myLastOne = myTotal - 1000;
+        System.out.println(myLastOne);
+
+        // Primiteve types
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println(myMinIntValue);
+        System.out.println(myMaxIntValue);
+        System.out.println("MyMax overflowed" + (myMaxIntValue + 1));
+        // if you designate it directly
+
+        int myMax = 2_147_483_647;
+
+        // Bytes short long and width
+        byte myByteMax = Byte.MAX_VALUE; // 8 bits hense the width of 8
+        byte myByteMin = Byte.MIN_VALUE;
+        System.out.println("myByteMax");
+        System.out.println(myByteMax);
+        System.out.println("myByteMin");
+        System.out.println(myByteMin);
+
+        short myShortMax = Short.MAX_VALUE; // 16 bits  => width of 16
+        short myShortMin = Short.MIN_VALUE;
+
+        System.out.println(myShortMax);
+        System.out.println(myShortMin);
+
+        long mylongVal = 100L; // 64 bits => width of 64
+
+        long myLongMax = Long.MAX_VALUE;
+        long myLongMin = Long.MIN_VALUE;
+        System.out.println(myLongMax);
+        System.out.println(myLongMin);
+
+        // casting
+
+        byte myNewByteValue = (byte) (myByteMin / 2);
+        System.out.println(myNewByteValue);
+
+        short myNewShortValue = (short) (myShortMax / 2);
+        System.out.println(myNewShortValue);
+
+        // floating numbers
+                
+
+       
+   } 
+}
