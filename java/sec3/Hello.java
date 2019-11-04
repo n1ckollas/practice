7c7df210f648edcaca8443df2b33d52f97f0ec70
Nickolai <nick.rebez@gmail.com>
2019-11-04 15:53:04 -0500

    start of primitivatypes(Byte Short int Long)

--- java/sec3/Hello.java
+++ java/sec3/Hello.java
@@ -2,5 +2,19 @@ public class Hello{
    public static void main(String[] args){
        System.out.println("Hello, Nick");
        int myFirstNum = 5;
+
+       System.out.println(myFirstNum);
+       System.out.println("whatever");
+       int mySecondNum = (10 + 5) + (2 * 10);
+       System.out.println(mySecondNum);
+       int myThirdNum = 6;
+       int myTotal = myFirstNum + mySecondNum + myThirdNum;
+       System.out.println(myTotal);
+       int myLastOne = myTotal - 1000;
+       System.out.println(myLastOne);
+       
+       // Primiteve types
+       
+       
    } 
 }
