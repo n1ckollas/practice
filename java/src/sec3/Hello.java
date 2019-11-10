68e1b526da9e7f8f1eec940e0084abac54cf708d
Nickolai <nick.rebez@gmail.com>
2019-11-09 19:23:46 -0500

    Lingt calculating function for the third time.

--- java/src/sec3/Hello.java
+++ java/src/sec3/Hello.java
@@ -1,17 +1,15 @@
-class Hello{
+class Hello {
     public static void main(String args[]){
         int lightspeed;
         long days;
-        long seconds;
         long distance;
+        long seconds;
         
-        lightspeed = 186000;
         days = 1000;
-        seconds = 60 * 60 * 24 * days;
+        lightspeed = 186000;
+        seconds = days * 24 * 60 * 60;
         distance = lightspeed * seconds;
+        System.out.println("in +" + days + " days, light will travel this distance " + distance);
 
-        System.out.println("in " + days);
-        System.out.println("days light will travel about");
-        System.out.println(distance + " miles."); 
     }
 }
