42cc55d3ea911725bc36b4c4c9c30b3a39fe920d
Nickolai <nick.rebez@gmail.com>
2019-12-18 08:39:34 -0500

    Morning Java practice

--- java/src/sec3/Hello.java
+++ java/src/sec3/Hello.java
@@ -1,9 +1,18 @@
 class Hello {
     public static void main(String args[]){
-        char ch1, ch2;
-        ch1 = 88;
-        ch2 = 'Y';
-        System.out.println(ch1);
-        System.out.println(ch2);
+        byte b = 42;
+        char c = 'A';
+        short s = 1024;
+        int i = 50_000;
+        float f = 5.67f;
+        double d = .1234;
+        double result = (f * b) + (i / c) - (d * s);
+        System.out.println("result = " + result);
+        int month_days [];
+        month_days = new int[12];
+        month_days[1] = 28;
+        System.out.println(month_days[1]);
+        System.out.println(month_days[0]);
+
     }
 }
