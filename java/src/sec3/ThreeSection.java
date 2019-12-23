c5c46c15c40928c6e59bfdfa57395225f838ecf5
Nickolai <nick.rebez@gmail.com>
2019-12-23 18:42:53 -0500

    Eventing java practive.

--- java/src/sec3/ThreeSection.java
+++ java/src/sec3/ThreeSection.java
@@ -1,49 +1,24 @@
-class ThreeSection{
+class ThreeSection {
     public static void main(String args[]){
-        //primitive types
-        byte a = 127; // width 8
-        short b = 32_767; // width 16
-        int c = 2_147_483_647; // width 32
-        long d = 234L;// or 2  ** 63, width 64
-        float f = 5.67f;
-        double dbl = .1234;
+        byte bte = 127;
+        short shrt = 32_767;
+        int intgr = 2_147_483_647;
+        long lng = 2345L;
+        float flt = 5.67f;
+        double dbl = 0.234;
 
-        char chr = 'A'; // width 16
-        String str = "My name is Nick";
+        char c = 'A';
+        String str = "Nick";
         boolean bool = true;
 
-        // type precedence
-        double dbltype = (f * b) + (c / chr) - (dbl - b);
-        System.out.print(dbltype);
-        
-        // operators
-        int result = 1 + 2;
-        int prevreREs = result;
-        System.out.println("operators");
-        result = result -1;
-        System.out.println(result);
-        result = result * 10;
-        System.out.println(result);
-        result = result / 5;
-        System.out.println(result);
-        result = result % 3;
-        System.out.println(result);
-        
+        // type precdence 
+        double resOne = (bte * shrt) + (intgr / flt) - (flt / dbl);
+        System.out.println(resOne);
+        float resTwo = (float) resOne;
+        System.out.println(resTwo);
+        short result3 = (short) (shrt - bte);
+        System.out.println(result3);
 
-        // short hand ops
-        System.out.println("shorthand operators");
-        result ++;
-        System.out.println(result);
-        result --;
-        System.out.println(result);
-        result += 2;
-        System.out.println(result);
-        result *= 10;
-        System.out.println(result);
-        result /=3;
-        System.out.println(result);
-        result -=2;
-        System.out.println(result);
-        
+
     }
 }
