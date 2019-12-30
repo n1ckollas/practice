d0d73800e769442b56edec9da04f42bdd7f43584
Nickolai <nick.rebez@gmail.com>
2019-12-29 20:56:34 -0500

    Method overloading. finished.

--- java/src/sec4/FourSection.java
+++ java/src/sec4/FourSection.java
@@ -3,9 +3,26 @@ class FourSection{
         System.out.println("Hello world");
        
         boolean gameOver = true;
+        int score = 800;
         int levelCompleted = 5;
         int bonus = 100;
+
+        calculateScore( gameOver, score, levelCompleted, bonus);
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+        calculateScore( gameOver, score, levelCompleted, bonus);
+
+        int calcScore = calculateScore("Tim", 500);
+        System.out.println("New score is " + calcScore);
+        
+        calculateScore(75);
+
+        calculateScore();
+
+        
     }
+
     public static int calculateScore(boolean gameOver, int score, 
     int levelCompleted, int bonus){
         if (gameOver){
@@ -17,5 +34,18 @@ class FourSection{
         
     }
     // Method overloading
+    public static int calculateScore(String playerName, int score){
+        System.out.println("Player " + playerName + " scored "
+            + score + " points ");
+        return score * 1000;
+    }
+    public static int calculateScore(int score){
+        System.out.println("Unnamed player scored " + score + " points");
+        return score * 1000;
+    }
+    public static int calculateScore(){
+        System.out.println("No player name, no player score provided");
+        return 0;
+    }
     
 }
