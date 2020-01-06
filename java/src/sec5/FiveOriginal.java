387537a6e2e10876d86dec5e45e2d9ea657a10e5
Nickolai <nick.rebez@gmail.com>
2020-01-05 23:29:41 -0500

    Switch statement + the challenges.

--- java/src/sec5/FiveOriginal.java
+++ java/src/sec5/FiveOriginal.java
@@ -1,140 +1,142 @@
 class FiveOriginal{
     public static void main(String[] args){
+        int switchVal = 4;
 
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
-
-        // Methods
-        calculateScore(gameOver, score, levelCompleted, bonus);
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-        calculateScore(gameOver, score, levelCompleted, bonus);
-
-
-        int calcScore = calculateScore("Tim", 500);
-        System.out.println("New score is " + calcScore);
-
-        calculateScore(75);
-
-        calculateScore();
-
-        double centimeters = calcFeetAndInchesToCentimeters(6,0);
-        if(centimeters <0.0) {
-            System.out.println("Invalid parameters");
+        switch(switchVal){
+            case 1:
+                System.out.println("The value is 1");
+                break; 
+                // break statement must be present at all times.
+                // coment it out and see that case 1 AND case 2 will run.
+            case 2:
+                System.out.println("the value is 2 ");
+                break;
+            case 3:
+                System.out.println("teh value is 3 ");
+                break;
+            case 4:
+                System.out.println("the value is 4");
+                break;
+            default:
+                System.out.println("the value is out of range");
+                break;
         }
 
-        calcFeetAndInchesToCentimeters(157);
+        // Cahllenge
 
+        char testChar = 'C';
 
-        // Create a method called calcFeetAndInchesToCentimeters
-        // It needs to have two parameters.
-        // feet is the first parameter, inches is the 2nd parameter
-        //
-        // You should validate that the first parameter feet is >= 0
-        // You should validate that the 2nd parameter inches is
-        // >=0 and <=12
-        // return -1 from the method if either of the above is not true
-        //
-        // If the parameters are valid, then calculate how many centimetres
-        // comprise the feet and inches passed to this method and return
-        // that value.
-        //
-        // Create a 2nd method of the same name but with only one parameter
-        // inches is the parameter
-        // validate that its >=0
-        // return -1 if it is not true
-        // But if its valid, then calculate how many feet are in the inches
-        // and then here is the tricky part
-        // call the other overloaded method passing the correct 
-        // feet and inches
-        // calculated so that it can calculate correctly.
-        // hints: Use double for your number datatypes is 
-        // probably a good idea
-        // 1 inch = 2.54cm  and one foot = 12 inches
-        // use the link I give you to confirm your 
-        // code is calculating correctly.
-        // Calling another overloaded method just requires you to use the
-        // right number of parameters.
+        switch(testChar){
+            case 'A':
+                System.out.println("it's an A");
+                break;
+            case 'B':
+                System.out.println("it's a B");
+                break;
+            case 'C':
+                System.out.println("it's a C");
+                break;
 
-    }
-
-    public static void displayHighScorePosition(String playerName, 
-    int highScorePosition) {
-        System.out.println(playerName + " managed to get into position "
-                + highScorePosition + " on the high score table");
-    }
-
-    public static int calculateHighScorePosition(int playerScore) {
-
-        int position = 4;  // assuming position 4 will be returned
-
-        if(playerScore >= 1000) {
-            position = 1;
-        } else if(playerScore >= 500) {
-            position = 2;
-        } else if(playerScore >= 100) {
-            position = 3;
+            default:
+                System.out.println("i do not know what it is");
+                break;
         }
 
-        return position;
-    }
+            printNumberInWord(4);
+            System.out.println(isLeapYear(-1600));
+            System.out.println(isLeapYear(1600));
+            System.out.println(isLeapYear(2017));
+            System.out.println(isLeapYear(2000));
 
-    public static int calculateScore(boolean gameOver,int score, 
-    int levelCompleted, int bonus) {
+            // for loop
+            
 
-        if(gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            finalScore += 2000;
-            return finalScore;
         }
-
-        return -1;
-
-    }
-    public static double calcFeetAndInchesToCentimeters(double feet, 
-    double inches) {
-
-        if((feet <0) || ((inches <0) || (inches >12))) {
-            System.out.println("Invalid feet or inches parameters");
-            return -1;
+        public static void printNumberInWord(int param){
+            switch(param){
+                case 0:
+                    System.out.println("ZERO");
+                    break;
+                case 1:
+                    System.out.println("ONE");
+                    break;
+                case 2:
+                    System.out.println("TWO");
+                    break;
+                case 3:
+                    System.out.println("THREE");
+                    break;
+                case 4:
+                    System.out.println("FOUR");
+                    break;
+                case 5:
+                    System.out.println("FIVE");
+                    break;
+                case 6:
+                    System.out.println("SIX");
+                    break;
+                case 7:
+                    System.out.println("SEVEN");
+                    break;
+                case 8:
+                    System.out.println("EIGHT");
+                    break;
+                case 9:
+                    System.out.println("NINE");
+                    break;
+                default:
+                    System.out.println("OTHER");
+                    break;
+            }
+            
         }
-
-        double centimeters = (feet * 12) * 2.54;
-        centimeters += inches * 2.54;
-        System.out.println(feet + " feet, " + inches 
-        + " inches = " + centimeters + " cm");
-        return centimeters;
-    }
-
-    public static double calcFeetAndInchesToCentimeters(double inches) {
-
-        if(inches < 0) {
-            return -1;
+        public static boolean isLeapYear(int year){
+            if(year < 1 || year > 9999){
+                return false;
+            }
+            if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+                return true;
+            }
+            return false;
         }
+        public static int getDaysInMonth(int month, int year){
+            if((month < 1 || month > 12) || (year < 1 || year > 9999)){
+                return -1;
+            }
+            boolean isLeap = isLeapYear(year);
+            int febDays = isLeap ? 29 : 28;
 
-        double feet = (int) inches / 12;
-        double remainingInches = (int) inches % 12;
-        System.out.println(inches + " inches is equal to " 
-        + feet + " feet and " + remainingInches + " inches");
-        return calcFeetAndInchesToCentimeters(feet, remainingInches);
-    }
+            switch(month){
+                case 1:
+                    return 31;
+                case 2:
+                    return febDays;
+                case 3:
+                    return 31;
+                case 4:
+                    return 30;
+                case 5:
+                    return 31;
+                case 6:
+                    return 30;
+                case 7:
+                    return 31;
+                case 8:
+                    return 31;
+                case 9:
+                    return 30;
+                case 10:
+                    return 31;
+                case 11:
+                    return 30;
+                case 12:
+                    return 31;
+                default:
+                    return -1;
+            }
+        }
+        // For loop
 
-    public static int calculateScore(String playerName, int score) {
-        System.out.println("Player " + playerName + " scored " 
-        + score + " points");
-        return score * 1000;
-    }
 
-    public static int calculateScore(int score) {
-        System.out.println("Unnamed player scored " + score + " points");
-        return score * 1000;
-    }
 
-    public static int calculateScore() {
-        System.out.println("No player name, no player score.");
-        return 0;
     }
-}
