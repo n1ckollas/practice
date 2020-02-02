89d3fe2c0627fb8c87bd1d311a0b3797f6c7e5fe
Nickolai <nick.rebez@gmail.com>
2020-02-02 16:58:37 -0500

    routing includes dashboard

--- ang7/src/app/app-routing.module.ts
+++ ang7/src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from "./components/heroes/heroes.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+
+
 
 const routes: Routes = [
-  {path: 'heroes', component: HeroesComponent }
+  {path: '', redirectTo:'/dashboard', pathMatch:'full'},
+  {path: 'heroes', component: HeroesComponent },
+  {path: 'dashboard', component: DashboardComponent}
 ]
 
 @NgModule({
--- ang7/src/app/components/dashboard/dashboard.component.ts
+++ ang7/src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero }  from '../../models/hero'
+import { HeroService } from '../../services/hero.service'
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
 
-  constructor() { }
+  constructor(private heroService:HeroService) {}
 
   ngOnInit() {
+    this.getHeroes();
   }
-
+  getHeroes(){
+    this.heroService.getHeroes()
+      .subscribe(heroes => {
+        this.heroes.push(heroes[1])
+        this.heroes.push(heroes[2])
+        this.heroes.push(heroes[3])
+        this.heroes.push(heroes[4])
+        this.heroes.push(heroes[5])
+      }
+      )}
 }
