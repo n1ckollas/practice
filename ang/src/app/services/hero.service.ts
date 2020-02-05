2d21b1d816b3b8f4e1d8188538ce8cb16d9c7291
Nickolai <nick.rebez@gmail.com>
2020-02-04 21:57:26 -0500

    complete

---
+++ ang/src/app/services/hero.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HEROES } from '../models/mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessagesService } from './messages.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messagesService:MessagesService) { }
+
+  getHeroes(){
+    this.messagesService.add("Fetched:: Heroes")
+    return of(HEROES);
+  }
+  
+}
---
+++ ang/src/app/services/messages.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessagesService {
+  messages:String[] = [];
+  constructor() { }
+
+  add(message: String){
+    this.messages.push(message);
+  }
+  clear(){
+    this.messages = [];
+  }
+
+
+}
