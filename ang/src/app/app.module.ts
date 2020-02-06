a997388a2880f0a7b87091dc2cdae2790cde24d9
Nickolai <nick.rebez@gmail.com>
2020-02-05 20:29:38 -0500

    Router stripped.

--- ang/src/app/app.module.ts
+++ ang/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
@@ -17,7 +16,6 @@ import { MessagesComponent } from './components/messages/messages.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
   ],
   providers: [],
