f32f1e7a523fa630d4820af6c8d0bf368ea0172a
Nickolai <nick.rebez@gmail.com>
2020-02-15 23:52:52 -0500

    Inheritance from scratch.

--- java/src/sec6/inheritance/Main.java
+++ java/src/sec6/inheritance/Main.java
@@ -1,12 +1,7 @@
 package inheritance;
 public class Main{
     public static void main(String args[]){
-        Dog chapa = new Dog(2, 4, 1, 32, "Chapa", 6,7);
-        System.out.println(chapa.name);
-        chapa.eat();
+
     }
-
-
-
     
 }
