3685f8f027cd163486a38b3319df4a8a8555546d
Nickolai <nick.rebez@gmail.com>
2020-03-13 01:06:33 -0400

    arrays first lesson.

--- java/src/sec8/Main.java
+++ java/src/sec8/Main.java
@@ -1,15 +1,17 @@
 public class Main{
     public static void main(String[] args){
         int[] myIntArray = new int[10];
+        // myIntArray[0] = 1;
+
         int[] intArray2 = { 1,2,3,4,5,6,7,8,9,10};
 
-        for(int i=0; i < 10; i++){
+        for(int i=0; i < myIntArray.length; i++){
             myIntArray[i] = intArray2[i] * 2;
         }
-        for(int i=0; i < 10; i++){
+        for(int i=0; i < myIntArray.length; i++){
             System.out.println("Element i in myIntAray" + i + ",  value is " + myIntArray[i]);
             System.out.println("Element i in intArray" + i + ",  value is " + intArray2[i]);
-
+            System.out.println("--------------------------------------------------------");
         }
     }
 }
