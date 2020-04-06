5c5f9b499dbbd03f1525d66620d8bf6756301dcb
Nick Rebezniuk <nick.rebez@gmail.com>
2020-04-05 23:31:09 -0400

    Vehicle class, car is next.

---
+++ java/src/sec6.2/Vehicle.java
@@ -0,0 +1,13 @@
+
+public class Vehicle {
+    private int state;
+    private String type;
+
+    public Vehicle(){
+       this(0, "Vehicle"); 
+    }
+    public Vehicle(int state, String type){
+       this.state = state;
+       this.type = type; 
+    }
+}
