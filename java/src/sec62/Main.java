95f749c0d5489091c85eb5f9bc1e304e88937b2c
Nick Rebezniuk <nick.rebez@gmail.com>
2020-04-07 14:25:52 -0400

    start over.

--- java/src/sec62/Main.java
+++ java/src/sec62/Main.java
@@ -1,11 +1,5 @@
 public class Main{
     public static void main(String[] args){
-        Car bmw = new Car("bmw", "m5", 8);
-        Car bmw2 = new Car("bmw", "m5", 10);
-
-        bmw.getFullSpec();
-        bmw2.getFullSpec();
-        bmw.getState();
-        bmw2.getState();
+        
     }
 }
