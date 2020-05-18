8507b7b2b384f22285ea90305d73ecab2350536f
Nick Rebezniuk <nick.rebez@gmail.com>
2020-05-18 12:47:14 -0400

    string generator.

--- java/src/sec62/Main.java
+++ java/src/sec62/Main.java
@@ -1,5 +1,11 @@
 public class Main{
     public static void main(String[] args){
-        
+
+        double conEd = 68.97;
+        double natGrid = 20.11;
+        double sptrm = 74.99;
+
+        double each = (conEd + natGrid + sptrm) / 3;
+        System.out.println(each);
     }
 }
---
+++ python3/py/r.py
@@ -0,0 +1,3 @@
+import random, string
+x = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(18))
+print(x)
