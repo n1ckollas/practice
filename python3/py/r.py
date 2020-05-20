6faa25e1a6400416ceada97bd9c4112069d07e2f
Nick Rebezniuk <nick.rebez@gmail.com>
2020-05-20 12:56:37 -0400

    updated string

--- python3/py/r.py
+++ python3/py/r.py
@@ -1,3 +1,4 @@
 import random, string
-x = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(18))
+
+x = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(24))
 print(x)
