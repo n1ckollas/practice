f0781e2469ef6cd30a4256a01f40395e2d285e7a
Mykola Rebezniuk <nick.rebez@gmail.com>
2020-09-16 21:29:10 -0400

    routes

--- ang72/src/app/app-routing.module.ts
+++ ang72/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HeroesComponent } from "./heroes/heroes.component";
+
+const routes: Routes = [
+  {path: 'heroes', component: HeroesComponent },
+]
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
