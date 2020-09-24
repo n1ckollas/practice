0be0154b11a3bfe697aa664c333e06fdc18dd658
Mykola Rebezniuk <nick.rebez@gmail.com>
2020-09-24 19:05:10 -0400

    Select a hero

--- ang73/src/app/heroes/heroes.component.ts
+++ ang73/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../types/hero.type';
-
+import {HEROES} from '../types/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -13,10 +13,16 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: "Windstorm",
   }
+  heroes: Hero[] = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelectHero(hero: Hero){
+    this.selectedHero = hero;
   }
 
 }
