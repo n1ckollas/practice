401e643d7f84b36619f65624e05c26b4202ddeb0
Mykola Rebezniuk <nick.rebez@gmail.com>
2020-09-24 20:42:06 -0400

    Dashboard + route.

--- ang73/src/app/app-routing.module.ts
+++ ang73/src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 const routes: Routes = [
-  {path:'heroes', component: HeroesComponent}
+  {path:'', redirectTo:'dashboard', pathMatch: 'full'},
+  {path:'heroes', component: HeroesComponent},
+  {path:'dashboard', component: DashboardComponent},
 ]
+
+
 @NgModule({
   declarations: [],
   imports: [
