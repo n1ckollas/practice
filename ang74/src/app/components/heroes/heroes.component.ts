d7786eaa47ac41ab5d6ec3c72e7a2a28e8948cda
Mykola Rebezniuk <nick.rebez@gmail.com>
2020-09-30 19:15:29 -0400

    Hero editor is finished.

--- ang74/src/app/components/heroes/heroes.component.ts
+++ ang74/src/app/components/heroes/heroes.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../types/hero.type';
+
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent implements OnInit {
 
+export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    name: "Windstorm",
+    id: 1,
+  }
   constructor() { }
 
   ngOnInit() {
