da1a647694609ae85058972dd8cd4732c45bcac3
Mykola Rebezniuk <nick.rebez@gmail.com>
2020-10-02 19:48:11 -0400

    No more mock heroes.

--- ang74/src/app/services/in-memory-data.service.ts
+++ ang74/src/app/services/in-memory-data.service.ts
@@ -6,7 +6,7 @@ import { Hero } from '../types/hero.type';
   providedIn: 'root'
 })
 
-export class InMemoryDataService {
+export class InMemoryDataService implements InMemoryDbService {
 
   createDb(){
     const heroes = [
@@ -27,6 +27,6 @@ export class InMemoryDataService {
   genId(heroes: Hero[]): number {
     return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
   }
-  
+
 
 }
