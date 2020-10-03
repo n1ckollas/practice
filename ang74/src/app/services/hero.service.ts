644b2243bde6e20002ccf78cd0762fac3d1229c8
Mykola Rebezniuk <nick.rebez@gmail.com>
2020-10-02 21:32:34 -0400

    Error handling for a single hero.

--- ang74/src/app/services/hero.service.ts
+++ ang74/src/app/services/hero.service.ts
@@ -22,6 +22,7 @@ export class HeroService {
   private log(message: string){
     this.messageService.add(`Hero Service ${message}`);
   }
+  
   private handlError<T>(operation = 'operation', result?: T){
     return (error: any): Observable<T> => {
       console.log(error);
@@ -32,10 +33,16 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
     this.messageService.add("Fetched all the heroes");
-    return this.httpClient.get<Hero[]>(this.heroUrl);
+    return this.httpClient.get<Hero[]>(this.heroUrl).pipe(
+      tap(_ => this.log("Fetched All heroes")),
+      catchError(this.handlError<Hero[]>('Fethcing All heroes failed', []))
+    )
   }
 
   getHero(id:number): Observable<Hero>{
-    return this.httpClient.get<Hero>(`${this.heroUrl}/${id}`);
+    return this.httpClient.get<Hero>(`${this.heroUrl}/${id}`).pipe(
+      tap(_ => this.log(`Fethced Hero  with id ${id}`)),
+      catchError(this.handlError<Hero>("Failed to fethc given hero"))
+    )
   }
 }
