eb4f2ab676180a960d8d8cbaa98bd12f8966d4fc
Mykola Rebezniuk <nick.rebez@gmail.com>
2020-10-07 19:43:43 -0400

    Master/Detail components.

--- ang75/src/app/components/hero-detail/hero-detail.component.ts
+++ ang75/src/app/components/hero-detail/hero-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Hero } from '../../types/hero.type';
+
 
 @Component({
   selector: 'app-hero-detail',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
 
   constructor() { }
 
