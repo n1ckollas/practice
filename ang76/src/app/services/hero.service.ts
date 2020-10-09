d4a476e1ede22d070283fb9c34cc57024d532f1c
Mykola Rebezniuk <nick.rebez@gmail.com>
2020-10-09 19:36:30 -0400

    Messages service implemented.

--- ang76/src/app/services/hero.service.ts
+++ ang76/src/app/services/hero.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { Hero } from '../types/hero.type';
 import { HEROES } from '../types/mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessagesService } from '../services/messages.service';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  constructor() { }
+  constructor( private messageService: MessagesService) { }
 
   getHeroes(): Observable<Hero[]>{
+    this.messageService.add("Fetched the heroes")
     return of(HEROES);
   }
 
