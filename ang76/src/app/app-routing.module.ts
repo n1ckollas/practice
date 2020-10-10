32e6ad350566e5cd854b7dae33362edc1df33690
Mykola Rebezniuk <nick.rebez@gmail.com>
2020-10-09 20:36:10 -0400

    details routing

---
+++ ang76/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
+
+const routes: Routes  = [
+    {path:'', redirectTo:'/heroes', pathMatch: 'full'},
+    {path:'heroes', component:HeroesComponent},
+    {path:'dashboard', component: DashboardComponent},
+    {path:'detail/:id', component:HeroDetailComponent},
+
+]
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+      RouterModule,
+  ]
+})
+export class AppRoutingModule { }
