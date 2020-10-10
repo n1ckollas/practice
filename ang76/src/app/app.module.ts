0704de4523f6d4c1258bb0f32c9dcb16971ad245
Mykola Rebezniuk <nick.rebez@gmail.com>
2020-10-09 21:51:06 -0400

    get all heroes and get one hero thorought http.

--- ang76/src/app/app.module.ts
+++ ang76/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
@@ -21,6 +24,8 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {dataEncapsulation: false})
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ang76/src/app/services/hero.service.ts
+++ ang76/src/app/services/hero.service.ts
@@ -1,25 +1,50 @@
 import { Injectable } from '@angular/core';
 import { Hero } from '../types/hero.type';
-import { HEROES } from '../types/mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessagesService } from '../services/messages.service';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  constructor( private messageService: MessagesService) { }
+  private heroesUrl = 'api/heroes'; 
+  private httpOptions = {
+    headers: new HttpHeaders({"Content-Type": 'application/json'}),
+  }
+
+  constructor( 
+    private messageService: MessagesService,
+    private http: HttpClient,
+  ) { }
+
+  private log(message: string): void{
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handlError<T>(operation = 'operation', result?: T){
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed with error: ${error.message}`)
+      return of(result as T);
+    }
+
+  }
 
   getHeroes(): Observable<Hero[]>{
-    this.messageService.add("Fetched the heroes")
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap( _ => this.log("Got All heroes")),
+      catchError(this.handlError<Hero[]>('Error while getting All heroes'))
+    )
   }
 
   getHero(id: number): Observable<Hero>{
-    const hero = HEROES.find(h => h.id === id);
-    this.messageService.add(`Found hero => ${hero.name}`);
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap((hero: Hero) => this.log(`found Hero => ${hero.name}`)),
+      catchError(this.handlError<Hero>("Found Nothing"))
+    )
   }
 
 }
