329d93cf0b6d5ea585d31484681f1d33616a4f65
Mykola Rebezniuk <nick.rebez@gmail.com>
2020-10-14 18:29:52 -0400

    Hero type. doble side binding.

--- ang78/src/app/components/heroes/heroes.component.ts
+++ ang78/src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../types/hero.type';
+
 
 @Component({
   selector: 'app-heroes',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  hero: Hero = {name: 'windsor', id:33};
 
   constructor() { }
 
