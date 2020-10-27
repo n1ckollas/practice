8c0eb24cdf0365998219ec64f1aa5738e31e053a
Mykola Rebezniuk <nick.rebez@gmail.com>
2020-10-26 21:14:48 -0400

    in memory api service.

--- ang1010/src/app/app.module.ts
+++ ang1010/src/app/app.module.ts
@@ -8,6 +8,10 @@ import { HeroDetailsComponent } from './components/hero-details/hero-details.com
 import { MessagesComponent } from './components/messages/messages.component';
 import { AppRoutingModule } from './routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryApiService } from './services/in-memory-api.service';
+
 
 @NgModule({
   declarations: [
@@ -21,6 +25,8 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryApiService, {dataEncapsulation: false})
   ],
   providers: [],
   bootstrap: [AppComponent]
