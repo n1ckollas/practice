cedd1325d526fbf39d37ec999d4ab2fd1c24f8c3
Mykola Rebezniuk <nick.rebez@gmail.com>
2020-11-01 00:45:54 -0400

    Reactive forms

--- ang7/src/app/components/form-builder/form-builder.component.ts
+++ ang7/src/app/components/form-builder/form-builder.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormArray } from '@angular/forms';
+
 
 @Component({
   selector: 'app-form-builder',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-builder.component.css']
 })
 export class FormBuilderComponent implements OnInit {
-
-  constructor() { }
+  profileForm = this.fb.group({
+    firstName:['', Validators.required],
+    lastName: [''],
+    address: this.fb.group({
+      street:[''],
+      city:[''],
+      state:[''],
+      zip:[''],
+    }),
+    aliases: this.fb.array([
+      this.fb.control(''),
+    ])
+  })
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+  }
+
+  get aliases(){
+    return this.profileForm.get('aliases') as FormArray;
+  }
+
+  addAlias(){
+    this.aliases.push(this.fb.control(''));
+  }
+
+  onSubmit(){
+    console.log(this.profileForm);
   }
 
 }
